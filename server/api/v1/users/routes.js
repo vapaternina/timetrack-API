615761329303f26494af698817ecd6bc2c9366d8
Jairo Diaz <jandradej@uninorte.edu.co>
2019-07-30 12:59:04 -0500

    readme changed

--- server/api/v1/users/routes.js
+++ server/api/v1/users/routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const controller = require("./controller");
 
+
 router.route("/").get(controller.all);
+
+
 
 module.exports = router;
--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@ require("dotenv").config("");
 const config = {
   server: {
     hostname: process.env.SERVER_IP,
-    port: process.env.PORT
+    port: process.env.SERVER_PORT,
   },
   db: {
     uri: process.env.DB_URI
